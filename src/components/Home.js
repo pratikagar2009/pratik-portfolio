03f438f25636defd3c1aacfcaeaaf213327cfa15
Pratik Agarwal <pratikagar2009@gmail.com>
2021-03-09 19:15:30 +0530

    portfolio complete

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import image from "../desktop-background.jpg";
+
+export default function Home() {
+    return (
+        <main>
+            <img src={image} alt="desktop Background" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg: leading-snug home-name">
+                    Aloha. I'm Pratik.
+                </h1>
+            </section>
+        </main>
+    )
+}
